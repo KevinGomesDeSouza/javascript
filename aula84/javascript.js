6d5e61b26c596e126a2e5e40cf3177ba5e504d6d
Kevin Gomes de Souza <116975028+KevinGomesDeSouza@users.noreply.github.com>
2024-11-06 17:08:00 -0300

    aula 84

--- aula84/javascript.js
+++ aula84/javascript.js
@@ -7,24 +7,33 @@ const configdv = {
 
 
 const dgv = (configdv)=>{
+    const dgvDados = document.querySelector(".dgvDados")
+    dgvDados.innerHTML = ''
     fetch(configdv.endpoint)
     .then(res=>res.json())
     .then(res =>{
     res.map((objeto)=>{
         //criar elementos
-        const dgvLinha = document.createElement("div","")
+        const dgvLinha = document.createElement("div")
         const id = document.createElement("div" )
         const produto = document.createElement("div")
         const marca = document.createElement("div")
         const modelo = document.createElement("div")
         const funcao = document.createElement("div")
+        const imgDeletar = document.createElement("img")
+        const imgEditar = document.createElement("img")
+        const imgExibir = document.createElement("img")
 
-        //passar os valores do JSON para eles
+        //passar os valores do JSON e os icones para eles
         id.innerHTML = objeto.id
         produto.innerHTML = objeto.produto
         marca.innerHTML = objeto.marca
         modelo.innerHTML = objeto.modelo
-        funcao.innerHTML = "D E V"
+        // funcao.innerHTML = "D E V"
+        imgDeletar.setAttribute("src","delete.svg")
+        imgEditar.setAttribute("src","edit.svg")
+        imgExibir.setAttribute("src","exibir.svg")
+
         //colocar os identificadores (classes)
         dgvLinha.setAttribute("class","dgvLinha")
         id.setAttribute("class","c1")
@@ -32,6 +41,15 @@ const dgv = (configdv)=>{
         marca.setAttribute("class","c3")
         modelo.setAttribute("class","c4")
         funcao.setAttribute("class","c5")
+        imgDeletar.setAttribute("class","dgvIcone")
+        imgEditar.setAttribute("class","dgvIcone")
+        imgExibir.setAttribute("class","dgvIcone")
+
+        //colocar os icones na coluna "função"
+
+        funcao.appendChild(imgDeletar)
+        funcao.appendChild(imgEditar)
+        funcao.appendChild(imgExibir)
 
 
         //colocar dentro da div linha
@@ -43,7 +61,7 @@ const dgv = (configdv)=>{
 
 
         //colocar no HTML
-        const dgvDados = document.querySelector(".dgvDados")
+        
         dgvDados.appendChild(dgvLinha)
         console.log(dgvLinha)
 
